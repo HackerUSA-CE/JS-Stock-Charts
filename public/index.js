594bbf07f48a32c313b26e28f7e85ad58d346b7e
Joshua Miles <joshua.m@hackerusa.com>
2021-06-18 16:53:27 -0500

    solution

--- public/index.js
+++ public/index.js
@@ -1,9 +1,94 @@
-async function main() {
+const colors = {
+    GME: 'rgba(61, 161, 61, 0.7)',
+    MSFT: 'rgba(209, 4, 25, 0.7)',
+    DIS: 'rgba(18, 4, 209, 0.7)',
+    BNTX: 'rgba(166, 43, 158, 0.7)'
+}
 
+async function main() {
     const timeChartCanvas = document.querySelector('#time-chart');
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
+    const response = await fetch(`https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1day&apikey=b2b0e11651294ee9a789558a1625b754`)
+
+    const result = await response.json()
+
+    const { GME, MSFT, DIS, BNTX } = result;
+
+    const stocks = [GME, MSFT, DIS, BNTX];
+
+    // Time Chart
+    new Chart(timeChartCanvas.getContext('2d'), {
+        type: 'line',
+        data: {
+            labels: stocks[0].values.reverse().map(value => value.datetime),
+            datasets: stocks.map(stock => ({
+                label: stock.meta.symbol,
+                backgroundColor: colors[stock.meta.symbol],
+                borderColor: colors[stock.meta.symbol],
+                data: stock.values.reverse().map(value => parseFloat(value.high))
+            }))
+        }
+    });
+
+    // High Chart
+    new Chart(highestPriceChartCanvas.getContext('2d'), {
+        type: 'bar',
+        data: {
+            labels: stocks.map(stock => stock.meta.symbol),
+            datasets: [{
+                label: 'Average',
+                backgroundColor: stocks.map(stock => (
+                    colors[stock.meta.symbol]
+                )),
+                borderColor: stocks.map(stock => (
+                    colors[stock.meta.symbol]
+                )),
+                data: stocks.map(stock => (
+                    findHighest(stock.values)
+                ))
+            }]
+        }
+    });
+
+    // Average Chart
+    new Chart(averagePriceChartCanvas.getContext('2d'), {
+        type: 'pie',
+        data: {
+            labels: stocks.map(stock => stock.meta.symbol),
+            datasets: [{
+                label: 'Average',
+                backgroundColor: stocks.map(stock => (
+                    colors[stock.meta.symbol]
+                )),
+                borderColor: stocks.map(stock => (
+                    colors[stock.meta.symbol]
+                )),
+                data: stocks.map(stock => (
+                    calculateAverage(stock.values)
+                ))
+            }]
+        }
+    });
+}
+
+function findHighest(values) {
+    let highest = 0;
+    values.forEach(value => {
+        if (parseFloat(value.high) > highest) {
+            highest = value.high
+        }
+    })
+    return highest
+}
+
+function calculateAverage(values) {
+    let total = 0;
+    values.forEach(value => {
+        total += parseFloat(value.high)
+    })
+    return total / values.length
 }
 
 main()
