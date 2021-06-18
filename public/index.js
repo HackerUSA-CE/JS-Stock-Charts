3fea2f552315f6cf9e00cd596bc1018d580d087b
Joshua Miles <joshua.m@hackerusa.com>
2021-06-18 15:50:31 -0500

    in progress

--- public/index.js
+++ public/index.js
@@ -1,5 +1,9 @@
 async function main(){
 
+    let timeChart = document.querySelector('#time-chart');
+    let highestPriceChart = document.querySelector('#highest-price-chart');
+    let averagePriceChart = document.querySelector('#highest-price-chart');
+    
     
 }
 
