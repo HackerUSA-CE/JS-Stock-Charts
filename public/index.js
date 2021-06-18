f9e18dd0e570dc92eec22306a90781d491880fa7
Joshua Miles <joshua.m@hackerusa.com>
2021-06-18 16:52:29 -0500

    finished starter code

--- public/index.js
+++ public/index.js
@@ -1,9 +1,9 @@
-async function main(){
+async function main() {
 
     const timeChartCanvas = document.querySelector('#time-chart');
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
-    const averagePriceChartCanvas = document.querySelector('#highest-price-chart');
-    
+    const averagePriceChartCanvas = document.querySelector('#average-price-chart');
+
 }
 
 main()
